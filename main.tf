12de2259ddd0366f31575e8fdc3d22153cbf01bf
Joe Tangoan <jessejoe.tangoan@daipost.com>
2025-06-19 13:37:23 +0800

    jht

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "blog" {
   ami = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.security-group.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
     tags = {
     Name = "jht-terraforms"
   }
@@ -82,7 +83,8 @@ module "security-group" {
   version = "5.3.0"
   name = "jht-new"
 
-  vpc_id = data.aws_vpc.default.id
+  # vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules =["all-all"]
@@ -100,3 +102,27 @@ module "security-group" {
 #     Name = "subs"
 #   }
 # }
+
+#######################
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jht-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+#####################

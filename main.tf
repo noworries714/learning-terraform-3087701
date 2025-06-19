484981a3d69c450d14a7843ef5f81a739f884cb8
Joe Tangoan <jessejoe.tangoan@daipost.com>
2025-06-19 10:29:18 +0800

    jht

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ vpc_security_group_ids = [aws_security_group.blog.id]
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allo http and https. Allow everything out"
-  vpc_id = data.aws_vpc.default
+  vpc_id = data.aws_vpc.default.id
 
 }
 

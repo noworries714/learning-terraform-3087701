cfa33dec5f7893131c54091c9bcd80c4329c2592
Joe Tangoan <jessejoe.tangoan@daipost.com>
2025-06-19 13:39:18 +0800

    jht

--- main.tf
+++ main.tf
@@ -14,12 +14,41 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+
+
+#######################
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jht-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+#####################
+
+
+
+
 ##VPC#######
 
 data "aws_vpc" "default" {
   default = true
 
 }
+
+
 
 ###sg instance###
 
@@ -103,26 +132,3 @@ module "security-group" {
 #   }
 # }
 
-#######################
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "jht-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  # enable_nat_gateway = true
-  # enable_vpn_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-
-
-#####################

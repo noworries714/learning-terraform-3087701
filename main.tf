0b1d8d4e25a2fa7664304ec125394743c8bbbf64
Joe Tangoan <jessejoe.tangoan@daipost.com>
2025-06-19 15:46:32 +0800

    load balancer

--- main.tf
+++ main.tf
@@ -48,8 +48,6 @@ data "aws_vpc" "default" {
 
 }
 
-
-
 ###sg instance###
 
 resource "aws_instance" "blog" {
@@ -59,6 +57,84 @@ resource "aws_instance" "blog" {
   subnet_id = module.vpc.public_subnets[0]
     tags = {
     Name = "jht-terraforms"
+  }
+}
+
+#### ALB ###
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+  name    = "jht-alb"
+  load_balancer_type = "applicaton"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  # Security Group
+  # security_group_ingress_rules = {
+  #   all_http = {
+  #     from_port   = 80
+  #     to_port     = 80
+  #     ip_protocol = "tcp"
+  #     description = "HTTP web traffic"
+  #     cidr_ipv4   = "0.0.0.0/0"
+  #   }
+  #   all_https = {
+  #     from_port   = 443
+  #     to_port     = 443
+  #     ip_protocol = "tcp"
+  #     description = "HTTPS web traffic"
+  #     cidr_ipv4   = "0.0.0.0/0"
+  #   }
+  # }
+  # security_group_egress_rules = {
+  #   all = {
+  #     ip_protocol = "-1"
+  #     cidr_ipv4   = "10.0.0.0/16"
+  #   }
+  # }
+
+  # access_logs = {
+  #   bucket = "my-alb-logs"
+  # }
+
+  http_tcp_listeners = [ 
+    { 
+      port = 80
+      protocol = http
+      target_group_index = 0
+    } 
+    ]
+  #   ex-https = {
+  #     port            = 443
+  #     protocol        = "HTTPS"
+  #     certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+  #     forward = {
+  #       target_group_key = "ex-instance"
+  #     }
+  #   }
+  # }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "jht-terraform-cloud"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id        = aws_instance.blog.id
+          port = 80
+        }
+      }
+     
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+
   }
 }
 

8291a5d914b65f31e0978d11f104fad82579e29e
Jessejoe Tangoan <tangoan.jht27@gmail.com>
2025-06-23 13:06:34 +0800

    jht

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -12,7 +12,7 @@ variable "ami_filter" {
   })
 
   default = {
-    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami-jht"
     owner = "979382823631" # Bitnami
   }
 }
